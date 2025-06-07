e1e047c3ab52dffd5da8b5a1be9006102c7bcfdd
carlitosvargas <carloscabj@gmail.com>
2025-06-07 12:38:28 -0300

    se realizo la asociacion de un usuario a una empresa- usaurioEmpresaController

--- controllers/usuarioEmpresaController.js
+++ controllers/usuarioEmpresaController.js
@@ -1,19 +1,29 @@
 const { Usuario, Empresa, UsuarioEmpresa} = require('../models');
 
 
-
 exports.asociarUsuarioEmpresa = async (req, res) => {
   try {
-    const { empresaId } = req.body;
+    const id = req.params.id;
+    const empresaId = req.params.empresaId;
 
-    const usuario = await Usuario.findByPk(req.params.id);
+    const usuario = await Usuario.findByPk(id);
     const empresa = await Empresa.findByPk(empresaId);
 
     if (!usuario || !empresa) {
       return res.status(404).json({ mensaje: 'Usuario o empresa no encontrados' });
     }
 
-    await usuario.addEmpresa(empresa); 
+    // Verifico en la tabla usuarioEmpresa si ya está asociado
+      const asociacionExistente = await UsuarioEmpresa.findOne({
+      where: { id_usuario: id }
+    });
+
+    if (asociacionExistente) {
+      return res.status(400).json({ mensaje: 'El usuario ya está asociado a otra empresa. Debe desvincularse primero.' });
+    }
+
+    // Si no está asociado, asocio
+    await usuario.addEmpresa(empresa);
 
     res.status(200).json({ mensaje: 'Usuario asociado a la empresa correctamente' });
   } catch (error) {
@@ -22,20 +32,52 @@ exports.asociarUsuarioEmpresa = async (req, res) => {
   }
 };
 
+exports.desasociarUsuarioEmpresa = async (req, res) => {
+  try {
+    const id = req.params.id; // id del usuario
+
+    // Buscar al usuario
+    const usuario = await Usuario.findByPk(id);
+
+    if (!usuario) {
+      return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+    }
+
+    // Verificar si está asociado a alguna empresa
+    const asociacion = await UsuarioEmpresa.findOne({
+      where: { id_usuario: id },
+    });
+
+    if (!asociacion) {
+      return res.status(400).json({ mensaje: 'El usuario no está asociado a ninguna empresa' });
+    }
+
+    // Eliminar la asociación
+    await UsuarioEmpresa.destroy({
+      where: { id_usuario: id },
+    });
+
+    res.status(200).json({ mensaje: 'Usuario desasociado de la empresa correctamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al desasociar usuario de la empresa' });
+  }
+};
+
 
 exports.obtenerEmpresaDeUsuario = async (req, res) => {
     const usuarioId = req.params.id; // ID del usuario recibido por parámetro
-
+    
     try {
         const asociacion = await UsuarioEmpresa.findOne({
-            where: { usuario_id: usuarioId }
+            where: { id_usuario: usuarioId }
         });
-
+     
         if (!asociacion) {
-            return res.status(404).json({ mensaje: 'El usuario no está asociado a ninguna empresa' });
+            return res.status(200).json({ mensaje: 'El usuario no está asociado a ninguna empresa', empresa_id:null });
         }
 
-        return res.status(200).json({ empresa_id: asociacion.empresa_id });
+        return res.status(200).json({ empresa_id: asociacion.id_empresa });
     } catch (error) {
         console.error('Error al verificar asociación del usuario:', error);
         return res.status(500).json({ error: 'Error del servidor' });
--- routes/usuarioEmpresa.js
+++ routes/usuarioEmpresa.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
-//const { asociarUsuarioEmpresa } = require('../controllers/usuarioEmpresaController');
 const usuarioEmpresa = require('../controllers/usuarioEmpresaController');
 
 
 const { autenticarToken, permitirPerfiles } = require('../middlewares/authMiddleware');
 
-router.post('/asociar/:id', autenticarToken,permitirPerfiles('usuarioAdministrador','usuarioMostrador','usuarioEmpresa'),usuarioEmpresa.asociarUsuarioEmpresa);
+router.post('/asociar/:id/:empresaId', autenticarToken,permitirPerfiles('usuarioAdministrador','usuarioMostrador','usuarioEmpresa'),usuarioEmpresa.asociarUsuarioEmpresa);
 
 router.get('/empresaUsuario/:id',autenticarToken,permitirPerfiles('usuarioAdministrador','usuarioMostrador','usuarioEmpresa'), usuarioEmpresa.obtenerEmpresaDeUsuario);
+
+router.delete('/desasociar/:id',autenticarToken,permitirPerfiles('usuarioAdministrador'),usuarioEmpresa.desasociarUsuarioEmpresa);
 
 router.get('/obtenerUsuariosPorEmpresa/:id',autenticarToken,permitirPerfiles('usuarioAdministrador','usuarioMostrador','usuarioEmpresa'), usuarioEmpresa.obtenerUsuariosPorEmpresa);
 module.exports = router;
