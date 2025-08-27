7a5ad6db23caa226c26a14f6177b240a83fe65ac
Gonzalo Vallejos <gonza18av@gmail.com>
2025-08-27 10:09:10 -0300

    ahora si lo modifique el enlace de la linea 214

--- controllers/authController.js
+++ controllers/authController.js
@@ -211,8 +211,7 @@ const solicitarRecuperacion = async (req, res) => {
     usuario.recuperacionTokenExpira = expiracion;
     await usuario.save();
 
-    // const link = `https://reserva-minubus-m39k.onrender.com/api/auth/abrir-app/${token}`;
-    const link = `https://front-reservas-app.onrender.com/resetear/${token}`;
+    const link = `https://reserva-minubus-m39k.onrender.com/api/auth/abrir-app/${token}`;
 
     await transporter.sendMail({
       from: '"Reservas 🚌" <vyvreservas25@gmail.com>',
